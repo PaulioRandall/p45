b28bf065b5f1e532acb14eed22a26a0b600badb6
Paulio <pauliorandall@gmail.com>
2024-01-01 18:44:50 +0000

    Validate FreeGrid input

--- src/grid/FreeGrid.js
+++ src/grid/FreeGrid.js
@@ -1,26 +1,27 @@
-// FreeGrid represents a grid with a fixed size, spacing, and origin offset.
+// FreeGrid represents a grid with a fixed size, spacing (pixel distance
+// between nodes), and origin offset.
+//
+// You can also view a grid as a square graph of nodes as the grid does not
+// busy itself with cells.
 //
 // Grids are usually built with an origin in the top left, bottom left, or
 // center. FreeGrids have their origin in the top left but can be offset such
 // that the passed coordinates are translated internally rather than the user
-// having to translate the input and output.
+// having to translate the inputs and outputs.
 //
 // This makes FreeGrids a nice tool for embedding in larger grids. Think of a
 // relatively placed set of vectors that can are translated together.
 //
 // FreeGrid instances are immutable.
 export default class FreeGrid {
-	// parseNumber parses n into a number if it can, else it returns NaN.
-	static parseNumber(n) {
-		if (typeof n === 'number') {
-			return n
-		}
-
-		if (typeof n === 'string') {
-			return Number(n)
-		}
-
-		return NaN
+	// checkArgs returns a string error message if any argument cannot be used
+	// to construct a FreeGrid.
+	static checkArgs(size, spacing, origin) {
+		return (
+			FreeGrid.checkSize(size) || //
+			FreeGrid.checkSpacing(spacing) || //
+			FreeGrid.checkOrigin(origin)
+		)
 	}
 
 	// checkSize returns a string error message if the size argument cannot be
@@ -34,17 +35,61 @@ export default class FreeGrid {
 			return `Requires grid size >= 3`
 		}
 
-		if (size > 99) {
-			return `Requires grid size <= 99`
+		return null
+	}
+
+	// checkSpacing returns a string error message if the spacing argument cannot
+	// be used to construct a FreeGrid.
+	static checkSpacing(spacing) {
+		if (spacing % 2 !== 0) {
+			return `Requires even numbered grid spacing`
+		}
+
+		if (spacing < 2) {
+			return `Requires grid spacing >= 3`
 		}
 
 		return null
 	}
 
+	// checkOrigin returns a string error message if the origin argument cannot
+	// be used to construct a FreeGrid.
+	static checkOrigin(origin) {
+		if (!origin || typeof origin !== 'object') {
+			return `Requires grid origin be an object`
+		}
+
+		const x = FreeGrid.parseNumber(origin.x)
+		if (isNaN(x)) {
+			return `Requires grid origin.x be a number or parsable number`
+		}
+
+		const y = FreeGrid.parseNumber(origin.y)
+		if (isNaN(y)) {
+			return `Requires grid origin.y be a number or parsable number`
+		}
+
+		return null
+	}
+
+	// parseNumber parses n into a number if it can, else it returns NaN.
+	static parseNumber(n) {
+		if (typeof n === 'number') {
+			return n
+		}
+
+		if (typeof n === 'string') {
+			return Number(n)
+		}
+
+		return NaN
+	}
+
 	constructor(size, spacing = 2, origin = { x: 0, y: 0 }) {
-		// TODO: Validate input
-		// TODO: odd sizes only
-		// TODO: even spacings >1 only
+		const e = FreeGrid.checkArgs(size, spacing, { ...origin })
+		if (e !== null) {
+			throw new Error(`[P45:FreeGrid:constructor] ${e}`)
+		}
 
 		this._lastIdx = size - 1
 		this._centerIdx = this._lastIdx / 2
@@ -90,20 +135,39 @@ export default class FreeGrid {
 		Object.freeze(this)
 	}
 
-	// contains returns true if the node identified by col and row is contained
+	parseNumber() {
+		return FreeGrid.parseNumber(...arguments)
+	}
+
+	// contains returns true if the node identified by x and y is contained
 	// within the grid.
-	contains(col, row) {
+	contains(x, y) {
 		// TODO
 	}
 
 	// node returns a node object containing properties relevant to the node
-	// identified by col and row.
-	node(col, row) {
+	// identified by x and y.
+	//
+	// offX and offY are optional pixel offsets applied relative to the node.
+	node(x = 0, y = 0, offX = 0, offY = 0) {
 		// TODO
+	}
+
+	// nodeXY returns a node object containing properties relevant to the node
+	// identified by xy.
+	//
+	// off contains optional pixel offsets applied relative to the node.
+	nodeXY(xy = { x: 0, y: 0 }, off = { x: 0, y: 0 }) {
+		return this.node(xy.x, xy.y, off.x, off.y)
 	}
 
 	// n is shorthand alias for the node function.
 	n() {
+		return this.node(...arguments)
+	}
+
+	// nXY is shorthand alias for the nodeXY function.
+	nXY() {
 		return this.node(...arguments)
 	}
 }
--- src/grid/FreeGrid.test.js
+++ src/grid/FreeGrid.test.js
@@ -1,50 +1,129 @@
 import FreeGrid from './FreeGrid.js'
 
 describe('FreeGrid.js', () => {
+	test('Static function checkArgs', () => {
+		let err
+
+		err = FreeGrid.checkArgs(3, 2, { x: 0, y: 0 })
+		expect(err).toEqual(null)
+
+		// size
+
+		err = FreeGrid.checkArgs(999, 2, { x: 0, y: 0 })
+		expect(err).toEqual(null)
+
+		err = FreeGrid.checkArgs(2, 2, { x: 0, y: 0 })
+		expect(err).not.toEqual(null)
+
+		err = FreeGrid.checkArgs(100, 2, { x: 0, y: 0 })
+		expect(err).not.toEqual(null)
+
+		// spacing
+
+		err = FreeGrid.checkArgs(3, 200, { x: 0, y: 0 })
+		expect(err).toEqual(null)
+
+		err = FreeGrid.checkArgs(3, 1, { x: 0, y: 0 })
+		expect(err).not.toEqual(null)
+
+		err = FreeGrid.checkArgs(3, 3, { x: 0, y: 0 })
+		expect(err).not.toEqual(null)
+
+		// origin
+
+		err = FreeGrid.checkArgs(3, 2, { x: 100, y: -100 })
+		expect(err).toEqual(null)
+
+		err = FreeGrid.checkArgs(3, 2, { x: '100', y: '100' })
+		expect(err).toEqual(null)
+
+		err = FreeGrid.checkArgs(3, 2, { x: 0 })
+		expect(err).not.toEqual(null)
+
+		err = FreeGrid.checkArgs(3, 2, 100)
+		expect(err).not.toEqual(null)
+
+		err = FreeGrid.checkArgs(3, 2, { x: 'abc', y: 0 })
+		expect(err).not.toEqual(null)
+	})
+
+	test('Static function parseNumber', () => {
+		let n
+
+		n = FreeGrid.parseNumber(2)
+		expect(n).toEqual(2)
+
+		n = FreeGrid.parseNumber('2')
+		expect(n).toEqual(2)
+
+		n = FreeGrid.parseNumber('abc')
+		expect(isNaN(n)).toEqual(true)
+
+		n = FreeGrid.parseNumber({})
+		expect(isNaN(n)).toEqual(true)
+
+		n = FreeGrid.parseNumber(BigInt(2))
+		expect(isNaN(n)).toEqual(true)
+	})
+
 	describe('GIVEN normal grid', () => {
 		const g = new FreeGrid(5, 2, { x: 0, y: 0 })
 
 		describe('THEN check property', () => {
-			test('grid._lastIdx', () => expect(g._lastIdx).toEqual(4))
-			test('grid._centerIdx', () => expect(g._centerIdx).toEqual(2))
+			test('grid._lastIdx', () => {
+				expect(g._lastIdx).toEqual(4)
+			})
 
-			test('grid.origin', () =>
+			test('grid._centerIdx', () => {
+				expect(g._centerIdx).toEqual(2)
+			})
+
+			test('grid.origin', () => {
 				expect(g.origin).toEqual({
 					x: 0, //
 					y: 0, //
-				}))
+				})
+			})
 
-			test('grid.lenXY', () => expect(g.lenXY).toEqual(5))
+			test('grid.lenXY', () => {
+				expect(g.lenXY).toEqual(5)
+			})
 
-			test('grid.centerXY', () =>
+			test('grid.centerXY', () => {
 				expect(g.centerXY).toEqual({
 					x: 2, //
 					y: 2, //
-				}))
+				})
+			})
 
-			test('grid.boundsXY', () =>
+			test('grid.boundsXY', () => {
 				expect(g.boundsXY).toEqual({
 					xMin: 0, //
 					xMax: 4, //
 					yMin: 0, //
 					yMax: 4, //
-				}))
+				})
+			})
 
-			test('grid.lenPx', () => expect(g.lenPx).toEqual(8))
+			test('grid.lenPx', () => {
+				expect(g.lenPx).toEqual(8)
+			})
 
-			test('grid.centerPx', () =>
+			test('grid.centerPx', () => {
 				expect(g.centerPx).toEqual({
 					x: 4, //
 					y: 4, //
-				}))
+				})
+			})
 
-			test('grid.boundsPx', () =>
+			test('grid.boundsPx', () => {
 				expect(g.boundsPx).toEqual({
 					xMin: 0, //
 					xMax: 8, //
 					yMin: 0, //
 					yMax: 8, //
-				}))
+				})
+			})
 		})
 	})
 
@@ -52,46 +131,59 @@ describe('FreeGrid.js', () => {
 		const g = new FreeGrid(5, 2, { x: -2, y: -2 })
 
 		describe('THEN check property', () => {
-			test('grid._lastIdx', () => expect(g._lastIdx).toEqual(4))
-			test('grid._centerIdx', () => expect(g._centerIdx).toEqual(2))
+			test('grid._lastIdx', () => {
+				expect(g._lastIdx).toEqual(4)
+			})
+			test('grid._centerIdx', () => {
+				expect(g._centerIdx).toEqual(2)
+			})
 
-			test('grid.origin', () =>
+			test('grid.origin', () => {
 				expect(g.origin).toEqual({
 					x: -2, //
 					y: -2, //
-				}))
+				})
+			})
 
-			test('grid.lenXY', () => expect(g.lenXY).toEqual(5))
+			test('grid.lenXY', () => {
+				expect(g.lenXY).toEqual(5)
+			})
 
-			test('grid.centerXY', () =>
+			test('grid.centerXY', () => {
 				expect(g.centerXY).toEqual({
 					x: 0, //
 					y: 0, //
-				}))
+				})
+			})
 
-			test('grid.boundsXY', () =>
+			test('grid.boundsXY', () => {
 				expect(g.boundsXY).toEqual({
 					xMin: -2, //
 					xMax: 2, //
 					yMin: -2, //
 					yMax: 2, //
-				}))
+				})
+			})
 
-			test('grid.lenPx', () => expect(g.lenPx).toEqual(8))
+			test('grid.lenPx', () => {
+				expect(g.lenPx).toEqual(8)
+			})
 
-			test('grid.centerPx', () =>
+			test('grid.centerPx', () => {
 				expect(g.centerPx).toEqual({
 					x: 0, //
 					y: 0, //
-				}))
+				})
+			})
 
-			test('grid.boundsPx', () =>
+			test('grid.boundsPx', () => {
 				expect(g.boundsPx).toEqual({
 					xMin: -4, //
 					xMax: 4, //
 					yMin: -4, //
 					yMax: 4, //
-				}))
+				})
+			})
 		})
 	})
 
@@ -99,46 +191,59 @@ describe('FreeGrid.js', () => {
 		const g = new FreeGrid(5, 2, { x: 2, y: 2 })
 
 		describe('THEN check property', () => {
-			test('grid._lastIdx', () => expect(g._lastIdx).toEqual(4))
-			test('grid._centerIdx', () => expect(g._centerIdx).toEqual(2))
+			test('grid._lastIdx', () => {
+				expect(g._lastIdx).toEqual(4)
+			})
+			test('grid._centerIdx', () => {
+				expect(g._centerIdx).toEqual(2)
+			})
 
-			test('grid.origin', () =>
+			test('grid.origin', () => {
 				expect(g.origin).toEqual({
 					x: 2, //
 					y: 2, //
-				}))
+				})
+			})
 
-			test('grid.lenXY', () => expect(g.lenXY).toEqual(5))
+			test('grid.lenXY', () => {
+				expect(g.lenXY).toEqual(5)
+			})
 
-			test('grid.centerXY', () =>
+			test('grid.centerXY', () => {
 				expect(g.centerXY).toEqual({
 					x: 4, //
 					y: 4, //
-				}))
+				})
+			})
 
-			test('grid.boundsXY', () =>
+			test('grid.boundsXY', () => {
 				expect(g.boundsXY).toEqual({
 					xMin: 2, //
 					xMax: 6, //
 					yMin: 2, //
 					yMax: 6, //
-				}))
+				})
+			})
 
-			test('grid.lenPx', () => expect(g.lenPx).toEqual(8))
+			test('grid.lenPx', () => {
+				expect(g.lenPx).toEqual(8)
+			})
 
-			test('grid.centerPx', () =>
+			test('grid.centerPx', () => {
 				expect(g.centerPx).toEqual({
 					x: 8, //
 					y: 8, //
-				}))
+				})
+			})
 
-			test('grid.boundsPx', () =>
+			test('grid.boundsPx', () => {
 				expect(g.boundsPx).toEqual({
 					xMin: 4, //
 					xMax: 12, //
 					yMin: 4, //
 					yMax: 12, //
-				}))
+				})
+			})
 		})
 	})
 })
