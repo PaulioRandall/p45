f04f62665a8ec2cde2a3cd68b4da323a37926307
Paulio <PaulioRandall@gmail.com>
2024-05-30 20:50:36 +0100

    Add demo page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,23 +7,36 @@
 </script>
 
 <main>
-	<div class="container">
-		<SVG {grid}>
-			<Circle o="I8" r="4" fill="red" />
-		</SVG>
-	</div>
+	<section class="icon-grid">
+		<div class="container">
+			<SVG {grid}>
+				<Circle o="I8" r="4" fill="red" />
+			</SVG>
+		</div>
+		<div class="container">
+			<SVG {grid}>
+				<Circle origin="I8" radius="4" fill="red" />
+			</SVG>
+		</div>
+	</section>
 </main>
 
 <style>
 	main {
 		width: 100dvw;
 		min-height: 100dvh;
-		padding: 1rem;
-		padding-bottom: 2rem;
+		padding: 2rem;
+	}
+
+	.icon-grid {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1rem;
 	}
 
 	.container {
-		width: 100px;
-		height: 100px;
+		width: 192px;
+		height: 192px;
+		border: 2px solid lightblue;
 	}
 </style>
