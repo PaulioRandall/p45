7e22f8f586db944506350281febdf065c4df9669
Paulio <PaulioRandall@gmail.com>
2024-06-16 23:18:06 +0100

    Stuff

--- src/p46/parser.js
+++ src/p46/parser.js
@@ -1,96 +1,126 @@
 import TokenReader from './TokenReader.js'
-import parseNode from './node-parser.js'
+import parseCoordNode from './node-parser.js'
 
-const parse = (cmd) => {
+const parse = (shape, cmd) => {
 	const r = new TokenReader(cmd)
 
 	if (r.is('move')) {
-		return parseMove(r)
+		return parseMove(shape, r)
+	}
+
+	if (r.is('close')) {
+		return parseClose(shape, r)
 	}
 
 	r.accept('draw')
-	return parseDraw(r)
+	return parseDraw(shape, r)
 }
 
-const parseMove = (r) => {
+const parseMove = (shape, r) => {
 	r.expect('move')
 	r.expect('to')
 
-	const n = parseNode(r.read())
+	const n = parseNode(shape, r)
+	shape.setStart(n)
 	return `M ${n.x} ${n.y}`
 }
 
-const parseDraw = (r) => {
+const parseClose = (shape, r) => {
+	r.expect('close')
+	r.accept('path')
+
+	shape.setPos(shape.getStart())
+	return `Z`
+}
+
+const parseDraw = (shape, r) => {
 	if (r.is('straight') || r.is('line')) {
-		return parseDrawStraightLine(r)
+		return parseDrawStraightLine(shape, r)
 	}
 
 	if (r.is('quadratic') || r.is('quad') || r.is('q')) {
-		return parseQuadraticCurve(r)
+		return parseQuadraticCurve(shape, r)
 	}
 
 	if (r.is('cubic') || r.is('curve')) {
-		return parseCubicCurve(r)
+		return parseCubicCurve(shape, r)
 	}
 
 	return []
 }
 
-const parseDrawStraightLine = (r) => {
+const parseDrawStraightLine = (shape, r) => {
 	r.accept('straight')
 	r.expect('line')
 	r.expect('to')
 
-	const n = parseNode(r.read())
+	const n = parseNode(shape, r)
+	shape.setPos(n)
 	return `L ${n.x} ${n.y}`
 }
 
-const parseQuadraticCurve = (r) => {
+const parseQuadraticCurve = (shape, r) => {
 	r.expect('q', 'quad', 'quadratic')
 	r.expect('curve')
 	r.expect('to')
 
-	const n = parseNode(r.read())
+	const n = parseNode(shape, r)
 
 	if (r.empty()) {
+		shape.setPos(n)
 		return `T ${n.x} ${n.y}`
 	}
 
 	r.expect('with')
 	r.expect('slope')
 
-	const cp = parseNode(r.read())
+	const cp = parseNode(shape, r)
+	shape.setPos(n)
 	return `Q ${cp.x} ${cp.y}, ${n.x} ${n.y}`
 }
 
-const parseCubicCurve = (r) => {
+const parseCubicCurve = (shape, r) => {
 	r.accept('cubic')
 	r.expect('curve')
 	r.expect('to')
 
-	const n = parseNode(r.read())
+	const n = parseNode(shape, r)
 
 	r.expect('with')
 
 	if (r.accept('slope')) {
-		return parseCubicSymmetricCurve(r, n)
+		return parseCubicSymmetricCurve(shape, r, n)
 	} else if (r.accept('slopes')) {
-		return parseCubicNonSymmetricCurve(r, n)
+		return parseCubicNonSymmetricCurve(shape, r, n)
 	}
 
 	throw new Error(`Unable to determine cubic curve parameters`)
 }
 
-const parseCubicSymmetricCurve = (r, n) => {
-	const cp = parseNode(r.read())
+const parseCubicSymmetricCurve = (shape, r, n) => {
+	const cp = parseNode(shape, r)
+	shape.setPos(n)
 	return `S ${cp.x} ${cp.y}, ${n.x} ${n.y}`
 }
 
-const parseCubicNonSymmetricCurve = (r, n) => {
-	const cp1 = parseNode(r.read())
+const parseCubicNonSymmetricCurve = (shape, r, n) => {
+	const cp1 = parseNode(shape, r)
 	r.expect('and')
-	const cp2 = parseNode(r.read())
+	const cp2 = parseNode(shape, r)
+	shape.setPos(n)
 	return `C ${cp1.x} ${cp1.y}, ${cp2.x} ${cp2.y}, ${n.x} ${n.y}`
+}
+
+const parseNode = (shape, r) => {
+	if (r.is('start')) {
+		return shape.getStart()
+	}
+
+	if (r.is('center')) {
+		return shape.getCenter()
+	}
+
+	return parseCoordNode(r.read())
 }
 
 export default parse
--- src/p46/parser.test.js
+++ src/p46/parser.test.js
@@ -1,50 +1,96 @@
 import parse from './parser.js'
+import ShapeState from './ShapeState.js'
 
 describe('parser.js', () => {
 	describe('parse', () => {
 		describe('move', () => {
 			test(`"move to D3"`, () => {
-				const act = parse(['move', 'to', 'D3'])
+				const shape = new ShapeState()
+
+				const act = parse(shape, [
+					'move', //
+					'to',
+					'D3',
+				])
+
 				expect(act).toEqual('M 3 3')
+				expect(shape.getStart().x).toEqual(3)
+				expect(shape.getStart().y).toEqual(3)
 			})
 		})
 
 		describe('straight line', () => {
 			test(`"line to D3"`, () => {
-				const act = parse(['line', 'to', 'D3'])
+				const act = parse(new ShapeState(), [
+					'line', //
+					'to',
+					'D3',
+				])
 				expect(act).toEqual('L 3 3')
 			})
 
 			test(`"straight line to D3"`, () => {
-				const act = parse(['straight', 'line', 'to', 'D3'])
+				const act = parse(new ShapeState(), [
+					'straight', //
+					'line',
+					'to',
+					'D3',
+				])
 				expect(act).toEqual('L 3 3')
 			})
 
 			test(`"draw line to D3"`, () => {
-				const act = parse(['draw', 'line', 'to', 'D3'])
+				const act = parse(new ShapeState(), [
+					'draw', //
+					'line',
+					'to',
+					'D3',
+				])
 				expect(act).toEqual('L 3 3')
 			})
 
 			test(`"draw straight line to D3"`, () => {
-				const act = parse(['draw', 'straight', 'line', 'to', 'D3'])
+				const act = parse(new ShapeState(), [
+					'draw', //
+					'straight',
+					'line',
+					'to',
+					'D3',
+				])
 				expect(act).toEqual('L 3 3')
 			})
 		})
 
 		describe('quadratic curve', () => {
 			test(`"q curve to D3 with slope D0"`, () => {
-				const act = parse(['q', 'curve', 'to', 'D3', 'with', 'slope', 'D0'])
+				const act = parse(new ShapeState(), [
+					'q', //
+					'curve',
+					'to',
+					'D3',
+					'with',
+					'slope',
+					'D0',
+				])
 				expect(act).toEqual('Q 3 0, 3 3')
 			})
 
 			test(`"quad curve to D3 with slope D0"`, () => {
-				const act = parse(['quad', 'curve', 'to', 'D3', 'with', 'slope', 'D0'])
+				const act = parse(new ShapeState(), [
+					'quad', //
+					'curve',
+					'to',
+					'D3',
+					'with',
+					'slope',
+					'D0',
+				])
 				expect(act).toEqual('Q 3 0, 3 3')
 			})
 
 			test(`"quadratic curve to D3 with slope D0"`, () => {
-				const act = parse([
-					'quadratic',
+				const act = parse(new ShapeState(), [
+					'quadratic', //
 					'curve',
 					'to',
 					'D3',
@@ -56,8 +102,8 @@ describe('parser.js', () => {
 			})
 
 			test(`"draw quadratic curve to D3 with slope D0"`, () => {
-				const act = parse([
-					'draw',
+				const act = parse(new ShapeState(), [
+					'draw', //
 					'quadratic',
 					'curve',
 					'to',
@@ -72,25 +118,41 @@ describe('parser.js', () => {
 
 		describe('symmetric quadratic curve', () => {
 			test(`"quadratic curve to D3"`, () => {
-				const act = parse(['quadratic', 'curve', 'to', 'D3'])
+				const act = parse(new ShapeState(), [
+					'quadratic', //
+					'curve',
+					'to',
+					'D3',
+				])
 				expect(act).toEqual('T 3 3')
 			})
 
 			test(`"quad curve to D3"`, () => {
-				const act = parse(['quad', 'curve', 'to', 'D3'])
+				const act = parse(new ShapeState(), [
+					'quad', //
+					'curve',
+					'to',
+					'D3',
+				])
 				expect(act).toEqual('T 3 3')
 			})
 
 			test(`"draw quadratic curve to D3"`, () => {
-				const act = parse(['draw', 'quadratic', 'curve', 'to', 'D3'])
+				const act = parse(new ShapeState(), [
+					'draw', //
+					'quadratic',
+					'curve',
+					'to',
+					'D3',
+				])
 				expect(act).toEqual('T 3 3')
 			})
 		})
 
 		describe('cubic curve', () => {
 			test(`"curve to D3 with slope C2 and D0"`, () => {
-				const act = parse([
-					'curve',
+				const act = parse(new ShapeState(), [
+					'curve', //
 					'to',
 					'D3',
 					'with',
@@ -103,8 +165,8 @@ describe('parser.js', () => {
 			})
 
 			test(`"draw curve to D3 with slope C2 and D0"`, () => {
-				const act = parse([
-					'draw',
+				const act = parse(new ShapeState(), [
+					'draw', //
 					'curve',
 					'to',
 					'D3',
@@ -118,15 +180,78 @@ describe('parser.js', () => {
 			})
 		})
 
-		describe('symmetric cubic curve', () => {
+		describe('symmetric quadratic cubic curve', () => {
 			test(`"curve to D3 with slope D0"`, () => {
-				const act = parse(['curve', 'to', 'D3', 'with', 'slope', 'D0'])
+				const act = parse(new ShapeState(), [
+					'curve', //
+					'to',
+					'D3',
+					'with',
+					'slope',
+					'D0',
+				])
 				expect(act).toEqual('S 3 0, 3 3')
 			})
 
 			test(`"draw curve to D3 with slope D0"`, () => {
-				const act = parse(['draw', 'curve', 'to', 'D3', 'with', 'slope', 'D0'])
+				const act = parse(new ShapeState(), [
+					'draw', //
+					'curve',
+					'to',
+					'D3',
+					'with',
+					'slope',
+					'D0',
+				])
 				expect(act).toEqual('S 3 0, 3 3')
+			})
+		})
+
+		describe('line/curve to start', () => {
+			test(`"line to start"`, () => {
+				const shape = new ShapeState()
+				shape.setStart({ x: 1, y: 1 })
+
+				const act = parse(shape, [
+					'line', //
+					'to',
+					'start',
+				])
+
+				expect(act).toEqual('L 1 1')
+			})
+		})
+
+		describe('line/curve to center', () => {
+			test(`"line to center"`, () => {
+				const shape = new ShapeState(100, 100)
+
+				const act = parse(shape, [
+					'line', //
+					'to',
+					'center',
+				])
+
+				expect(act).toEqual('L 50 50')
+			})
+		})
+
+		describe('close path', () => {
+			test(`"close path"`, () => {
+				const act = parse(new ShapeState(), [
+					'close', //
+					'path',
+				])
+
+				expect(act).toEqual('Z')
+			})
+
+			test(`"close path"`, () => {
+				const act = parse(new ShapeState(), [
+					'close', //
+				])
+
+				expect(act).toEqual('Z')
 			})
 		})
 	})
